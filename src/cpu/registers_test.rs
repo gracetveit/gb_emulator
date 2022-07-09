d7d5fe6c2048914c845f143f7ed817999c3767a2
Grace Tveit <grace.tveit@gmail.com>
2022-07-09 12:52:44 -0700

    test: adding tests for registers

--- src/cpu/registers_test.rs
+++ src/cpu/registers_test.rs
@@ -1,21 +1,85 @@
-use crate::cpu::registers::Registers;
+use super::registers::{Registers, FlagsRegister};
+
+const TEST_BITS: u16 = 0b1000110100101101;
+const TEST_UPPER_NIBBLE: u8 = 0b10001101;
+const TEST_LOWER_NIBBLE: u8 = 0b00101101;
 
 #[test]
-#[should_panic]
-fn init_with_fewer_than_8_registers() {
-    let registers: Vec<u8> = vec![0, 0, 0, 0, 0, 0, 0];
-    Registers::new(&registers);
+fn set_get_bc() {
+    let mut test_registers = Registers {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: FlagsRegister::from(0),
+        h: 0,
+        l: 0
+    };
+
+    test_registers.set_bc(TEST_BITS);
+    assert_eq!(test_registers.b, TEST_UPPER_NIBBLE);
+    assert_eq!(test_registers.c, TEST_LOWER_NIBBLE);
+
+    assert_eq!(test_registers.get_bc(), TEST_BITS);
 }
 
 #[test]
-#[should_panic]
-fn init_with_more_than_8_registers() {
-    let registers: Vec<u8> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
-    Registers::new(&registers);
+fn set_get_af() {
+    let mut test_registers = Registers {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: FlagsRegister::from(0),
+        h: 0,
+        l: 0
+    };
+
+    test_registers.set_af(TEST_BITS);
+    assert_eq!(test_registers.a, TEST_UPPER_NIBBLE);
+    assert_eq!(u8::from(test_registers.f), 0b00100000);
+
+    assert_eq!(test_registers.get_af(), 0b1000110100100000);
 }
 
-// #[test]
-// fn get_set_bc() {
-//     let test = Registers::new(vec![0, 0, 0, 0, 0, 0, 0, 0]);
-//     test.set_bc(3640)
-// }
+#[test]
+fn set_get_de(){
+    let mut test_registers = Registers {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: FlagsRegister::from(0),
+        h: 0,
+        l: 0
+    };
+
+    test_registers.set_de(TEST_BITS);
+    assert_eq!(test_registers.d, TEST_UPPER_NIBBLE);
+    assert_eq!(test_registers.e, TEST_LOWER_NIBBLE);
+
+    assert_eq!(test_registers.get_de(), TEST_BITS);
+}
+
+#[test]
+fn set_get_hl(){
+    let mut test_registers = Registers {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: FlagsRegister::from(0),
+        h: 0,
+        l: 0
+    };
+
+    test_registers.set_hl(TEST_BITS);
+    assert_eq!(test_registers.h, TEST_UPPER_NIBBLE);
+    assert_eq!(test_registers.l, TEST_LOWER_NIBBLE);
+
+    assert_eq!(test_registers.get_hl(), TEST_BITS);
+}
