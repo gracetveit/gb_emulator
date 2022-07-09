e794a5a87c120ee22073e4830d4490e80bae2fc5
Grace Tveit <grace.tveit@gmail.com>
2022-07-09 12:52:33 -0700

    feat: adding the other 16 bit registers and setting neccessary info to pub

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,37 +1,23 @@
 pub struct Registers {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    f: FlagsRegister,
-    h: u8,
-    l: u8
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: FlagsRegister,
+    pub h: u8,
+    pub l: u8
 }
 
 impl Registers {
-    pub fn new(reg: &Vec<u8>) -> Self {
-        if reg.len() != 8 {panic!()}
-
-        Registers {
-            a: reg[0],
-            b: reg[1],
-            c: reg[2],
-            d: reg[3],
-            e: reg[4],
-            f: FlagsRegister::from(reg[5]),
-            h: reg[6],
-            l: reg[7]
-        }
-    }
-    fn get_bc(&self) -> u16 {
+    pub fn get_bc(&self) -> u16 {
         // pushes the 8 digits of b to the last 8 significant digits, and using
         // a bitwise OR operator, adds c as the first 8 digits
         (self.b as u16) << 8
         | self.c as u16
     }
 
-    fn set_bc(&mut self, value: u16) {
+    pub fn set_bc(&mut self, value: u16) {
         // Uses a bitwise & operator to grab the last 8 significant digits of
         // value, and shifts them 8 'digits' backwards to make it an 8 bit
         // integer
@@ -40,11 +26,35 @@ impl Registers {
         self.c = (value & 0xFF) as u8;
     }
 
-    // TODO: af getter/setter
+    pub fn get_af(&self) -> u16 {
+        (self.a as u16) << 8
+        | (u8::from(self.f)) as u16
+    }
 
-    // TODO: de getter/setter
+    pub fn set_af(&mut self, value: u16) {
+        self.a = ((value & 0xFF00) >> 8) as u8;
+        self.f = FlagsRegister::from((value & 0xFF) as u8);
+    }
 
-    // TODO: hl getter/setter
+    pub fn get_de(&self) -> u16 {
+        (self.d as u16) << 8
+        | self.e as u16
+    }
+
+    pub fn set_de(&mut self, value: u16) {
+        self.d = ((value & 0xFF00) >> 8) as u8;
+        self.e = (value & 0xFF) as u8;
+    }
+
+    pub fn get_hl(&self) -> u16 {
+        (self.h as u16) << 8
+        | self.l as u16
+    }
+
+    pub fn set_hl(&mut self, value: u16) {
+        self.h = ((value & 0xFF00) >> 8) as u8;
+        self.l = (value & 0xFF) as u8;
+    }
 }
 
 const ZERO_FLAG_BYTE_POSTION: u8 = 7;
@@ -52,11 +62,12 @@ const SUBTRACT_FLAG_BYTE_POSTIONG: u8 = 6;
 const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
 const CARRY_FLAG_BYTE_POSITION: u8 = 4;
 
-struct FlagsRegister {
-    zero: bool,
-    subtract: bool,
-    half_carry: bool,
-    carry: bool
+#[derive(Copy, Clone)]
+pub struct FlagsRegister {
+    pub zero: bool,
+    pub subtract: bool,
+    pub half_carry: bool,
+    pub carry: bool
 }
 
 impl std::convert::From<FlagsRegister> for u8 {
