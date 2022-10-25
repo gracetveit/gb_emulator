b376b6a6fb930f75734b03f3ac2062f7dfe9fdde
Grace Tveit <grace.tveit@gmail.com>
2022-10-25 15:05:22 -0700

    tests: Adding more tests
    - AND
    - OR
    - XOR
    - INC
    - DEC
    
    Also adding outline for SBC test, but need to fix SBC (and probably SUB) first

--- src/cpu/cpu_test.rs
+++ src/cpu/cpu_test.rs
@@ -61,6 +61,89 @@ fn test_sub() {
     assert_eq!(test_cpu.registers.f.carry, false)
 }
 
+// #[test]
+// fn test_sbc() {
+//     let mut test_cpu = create_cpu(0u8, 5, FlagsRegister::from(0));
+
+//     test_cpu.execute(Instruction::SBC(ArithmeticTarget::B));
+
+//     assert_eq!(test_cpu.registers.a, 251);
+// }
+
+#[test]
+fn test_and() {
+    let mut test_cpu = create_cpu(0b11110000, 0b00010000, FlagsRegister::from(0));
+
+    test_cpu.execute(Instruction::AND(ArithmeticTarget::B));
+
+    assert_eq!(test_cpu.registers.a, 0b00010000);
+
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, false);
+    assert_eq!(test_cpu.registers.f.half_carry, true);
+    assert_eq!(test_cpu.registers.f.carry, false)
+}
+
+#[test]
+fn test_or() {
+    let mut test_cpu = create_cpu(0b11110000, 0b00000001, FlagsRegister::from(0));
+
+    test_cpu.execute(Instruction::OR(ArithmeticTarget::B));
+
+    assert_eq!(test_cpu.registers.a, 0b11110001);
+
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, false);
+    assert_eq!(test_cpu.registers.f.half_carry, false);
+    assert_eq!(test_cpu.registers.f.carry, false)
+}
+
+#[test]
+fn test_xor() {
+    let mut test_cpu = create_cpu(0b11110000, 0b11110001, FlagsRegister::from(0));
+
+    test_cpu.execute(Instruction::XOR(ArithmeticTarget::B));
+
+    assert_eq!(test_cpu.registers.a, 1);
+
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, false);
+    assert_eq!(test_cpu.registers.f.half_carry, false);
+    assert_eq!(test_cpu.registers.f.carry, false)
+}
+
+#[test]
+fn test_inc() {
+    let mut test_cpu = create_cpu(0, 0, FlagsRegister::from(0));
+
+    test_cpu.execute(Instruction::INC(ArithmeticTarget::A));
+
+    assert_eq!(test_cpu.registers.a, 1);
+
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, false);
+    assert_eq!(test_cpu.registers.f.half_carry, false);
+    assert_eq!(test_cpu.registers.f.carry, false)
+}
+
+#[test]
+fn test_dec() {
+    let mut test_cpu = create_cpu(
+        1,
+        0,
+        FlagsRegister::from(0)
+    );
+
+    test_cpu.execute(Instruction::DEC(ArithmeticTarget::A));
+
+    assert_eq!(test_cpu.registers.a, 0);
+
+    assert_eq!(test_cpu.registers.f.zero, true);
+    assert_eq!(test_cpu.registers.f.subtract, true);
+    assert_eq!(test_cpu.registers.f.half_carry, false);
+    assert_eq!(test_cpu.registers.f.carry, false);
+}
+
 #[test]
 fn test_rra() {
     let mut test_cpu = create_cpu(
