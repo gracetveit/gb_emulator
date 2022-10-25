560379bc211f5fb571b15b28310833efdbe38a25
Grace Tveit <grace.tveit@gmail.com>
2022-10-25 13:11:44 -0700

    tests: Adding more tests for add, adc, sub/n adding create_cpu helper function

--- src/cpu/cpu_test.rs
+++ src/cpu/cpu_test.rs
@@ -1,52 +1,78 @@
 use super::{
     cpu::CPU,
-    instruction::Instruction,
+    instruction::{ArithmeticTarget, Instruction},
     registers::{FlagsRegister, Registers},
 };
 
+fn create_cpu(a: u8, b: u8, f: FlagsRegister) -> CPU {
+    CPU {
+        registers: Registers {
+            a,
+            b,
+            c: 0,
+            d: 0,
+            e: 0,
+            f,
+            h: 0,
+            l: 0,
+        },
+    }
+}
+
 #[test]
 fn test_add() {
-    let test_registers = Registers {
-        a: 0,
-        b: 0,
-        c: 1,
-        d: 0,
-        e: 0,
-        f: FlagsRegister::from(0),
-        h: 0,
-        l: 0,
-    };
+    let mut test_cpu = create_cpu(15, 1, FlagsRegister::from(0));
 
-    let mut test_cpu = CPU {
-        registers: test_registers,
-    };
+    test_cpu.execute(Instruction::ADD(ArithmeticTarget::B));
 
-    test_cpu.execute(Instruction::ADD(super::instruction::ArithmeticTarget::C));
+    assert_eq!(test_cpu.registers.a, 16);
 
-    assert_eq!(test_cpu.registers.a, 1)
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, false);
+    assert_eq!(test_cpu.registers.f.half_carry, true);
+    assert_eq!(test_cpu.registers.f.carry, false);
+}
+
+#[test]
+fn test_adc() {
+    let mut test_cpu = create_cpu(255, 2, FlagsRegister::from(0));
+
+    test_cpu.execute(Instruction::ADC(ArithmeticTarget::B));
+
+    assert_eq!(test_cpu.registers.a, 2);
+
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, false);
+    assert_eq!(test_cpu.registers.f.half_carry, true);
+    assert_eq!(test_cpu.registers.f.carry, true);
+}
+
+#[test]
+fn test_sub() {
+    let mut test_cpu = create_cpu(10, 5, FlagsRegister::from(0));
+
+    test_cpu.execute(Instruction::SUB(ArithmeticTarget::B));
+
+    assert_eq!(test_cpu.registers.a, 5);
+
+    assert_eq!(test_cpu.registers.f.zero, false);
+    assert_eq!(test_cpu.registers.f.subtract, true);
+    assert_eq!(test_cpu.registers.f.half_carry, false);
+    assert_eq!(test_cpu.registers.f.carry, false)
 }
 
 #[test]
 fn test_rra() {
-    let test_registers = Registers {
-        a: 0b000010110,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: FlagsRegister {
+    let mut test_cpu = create_cpu(
+        0b000010110,
+        0,
+        FlagsRegister {
             zero: false,
             subtract: false,
             half_carry: false,
             carry: true,
         },
-        h: 0,
-        l: 0,
-    };
-
-    let mut test_cpu = CPU {
-        registers: test_registers,
-    };
+    );
 
     test_cpu.execute(Instruction::RRA);
 
@@ -56,25 +82,16 @@ fn test_rra() {
 
 #[test]
 fn test_rla() {
-    let test_registers = Registers {
-        a: 0b000010111,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: FlagsRegister {
+    let mut test_cpu = create_cpu(
+        0b000010111,
+        0,
+        FlagsRegister {
             zero: false,
             subtract: false,
             half_carry: false,
             carry: true,
         },
-        h: 0,
-        l: 0,
-    };
-
-    let mut test_cpu = CPU {
-        registers: test_registers,
-    };
+    );
 
     test_cpu.execute(Instruction::RLA);
 
@@ -84,25 +101,16 @@ fn test_rla() {
 
 #[test]
 fn test_rrca() {
-    let test_registers = Registers {
-        a: 0b000010111,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: FlagsRegister {
+    let mut test_cpu = create_cpu(
+        0b000010111,
+        0,
+        FlagsRegister {
             zero: false,
             subtract: false,
             half_carry: false,
             carry: true,
         },
-        h: 0,
-        l: 0,
-    };
-
-    let mut test_cpu = CPU {
-        registers: test_registers,
-    };
+    );
 
     test_cpu.execute(Instruction::RRCA);
 
@@ -112,25 +120,16 @@ fn test_rrca() {
 
 #[test]
 fn test_rrla() {
-    let test_registers = Registers {
-        a: 0b000010111,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: FlagsRegister {
+    let mut test_cpu = create_cpu(
+        0b000010111,
+        0,
+        FlagsRegister {
             zero: false,
             subtract: false,
             half_carry: false,
             carry: true,
         },
-        h: 0,
-        l: 0,
-    };
-
-    let mut test_cpu = CPU {
-        registers: test_registers,
-    };
+    );
 
     test_cpu.execute(Instruction::RRLA);
 
