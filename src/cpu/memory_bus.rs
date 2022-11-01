b9932ebe5c5d16eed7deb22738db3601d52b1323
Grace Tveit <grace.tveit@gmail.com>
2022-11-01 15:43:10 -0700

    feat: add the ability to read the bios

--- src/cpu/memory_bus.rs
+++ src/cpu/memory_bus.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 pub struct MemoryBus {
     pub in_bios: bool,
     bios: [u8; 0xFF + 1],
@@ -11,16 +13,57 @@ pub struct MemoryBus {
 
 impl MemoryBus {
     pub fn new() -> MemoryBus {
-        MemoryBus {
+        let mut new_bus = MemoryBus {
             in_bios: true,
-            bios: [0;0xFF + 1],
-            rom: [0;0x7FFF + 1],
+            bios: MemoryBus::read_bios(),
+            rom: [0; 0x8000],
             v_ram: [0; 0x2000],
             e_ram: [0; 0x2000],
             w_ram: [0; 0x5E00],
             z_ram: [0; 0x80],
-        }
+        };
+        // new_bus.read_rom();
+        new_bus
     }
+
+    fn read_bios() -> [u8; 0x100] {
+        let mut bios = [0; 0x100];
+        let file = fs::read("./roms/gb_bios.bin");
+        match file {
+            Ok(t) => {
+                let mut i: usize = 0;
+                for x in t.into_iter() {
+                    bios[i] = x;
+                    i += 1;
+                }
+            }
+            Err(e) => {
+                println!("Error: {e:?}")
+            }
+        }
+        bios
+    }
+
+    // TODO: Re-implement `read_rom` when all write_byte functionality is complete
+    // fn read_rom(&mut self){
+    //     let mut rom = [0; 0x8000];
+    //     let file = fs::read("./roms/zelda.gb");
+    //     let initial_bios_toggle = self.in_bios;
+    //     self.in_bios = false;
+    //     match file {
+    //         Ok(t) => {
+    //             let mut i: u16 = 0;
+    //             for x in t.into_iter() {
+    //                 self.write_byte(i, x);
+    //                 i += 1;
+    //             }
+    //         }
+    //         Err(e) => {
+    //             println!("Error: {e:?}")
+    //         }
+    //     }
+    //     self.in_bios = initial_bios_toggle;
+    // }
 
     pub fn read_byte(&self, addr: u16) -> u8 {
         match addr & 0xF000 {
@@ -33,36 +76,22 @@ impl MemoryBus {
                 }
             }
             // ROM0
-            0x1000..=0x3000 => {
-                self.rom[addr as usize]
-            }
+            0x1000..=0x3000 => self.rom[addr as usize],
             // ROM1
-            0x4000..=0x7000 => {
-                self.rom[addr as usize]
-            }
+            0x4000..=0x7000 => self.rom[addr as usize],
             // Graphics: VRAM
-            0x8000..=0x9000 => {
-                self.v_ram[(addr & 0x1FFF) as usize]
-            }
+            0x8000..=0x9000 => self.v_ram[(addr & 0x1FFF) as usize],
             // External RAM
-            0xA000..=0xB000 => {
-                self.e_ram[(addr & 0x1FFF) as usize]
-            }
+            0xA000..=0xB000 => self.e_ram[(addr & 0x1FFF) as usize],
             // Working Ram
-            0xC000..=0xD000 => {
-                self.w_ram[(addr & 0x1FFF) as usize]
-            }
+            0xC000..=0xD000 => self.w_ram[(addr & 0x1FFF) as usize],
             // Working Ram shadow
-            0xE000 => {
-                self.w_ram[(addr & 0x1FFF) as usize]
-            }
+            0xE000 => self.w_ram[(addr & 0x1FFF) as usize],
             // Working RAM shadow, I/o, Zero-page RAM
             0xF000 => {
                 match addr & 0x0F00 {
                     // Working RAM shadow
-                    0x000..=0xD00 => {
-                        self.w_ram[(addr & 0x1FFF) as usize]
-                    }
+                    0x000..=0xD00 => self.w_ram[(addr & 0x1FFF) as usize],
                     // Graphics: objets attribute memory
                     // OAM is 160 bytes, remaining bytes read as 0
                     0xE00 => {
@@ -82,10 +111,10 @@ impl MemoryBus {
                             0
                         }
                     }
-                    _ => panic!()
+                    _ => panic!(),
                 }
             }
-            _ => panic!()
+            _ => panic!(),
         }
     }
 
@@ -136,7 +165,6 @@ impl MemoryBus {
                         if addr < 0xFEA0 {
                             todo!() // TODO: Write GPU fn: oam
                         } else {
-
                         }
                     }
                     // Zero-page
@@ -148,10 +176,10 @@ impl MemoryBus {
                             // Currently unhandled
                         }
                     }
-                    _ => panic!()
+                    _ => panic!(),
                 }
             }
-            _ => panic!()
+            _ => panic!(),
         }
     }
 
@@ -171,14 +199,6 @@ impl MemoryBus {
 }
 
 #[cfg(test)]
-
-#[test]
-fn test_read_bios() {
-    let test_bus = MemoryBus::new();
-
-    let _ = test_bus.bios[0xFF];
-}
-
 #[test]
 fn write_read_byte_test() {
     let mut test_bus = MemoryBus::new();
@@ -189,7 +209,6 @@ fn write_read_byte_test() {
 
     test_bus.write_byte(test_addr, test_byte);
     assert_eq!(test_bus.read_byte(test_addr), test_byte)
-
 }
 
 #[test]
@@ -202,4 +221,17 @@ fn write_read_word_test() {
 
     test_bus.write_word(test_addr, test_word);
     assert_eq!(test_bus.read_word(test_addr), test_word)
+}
+#[test]
+fn memory_bus_init_test() {
+    MemoryBus::new();
+}
+
+#[test]
+fn read_bios_test() {
+    let test_bus = MemoryBus::new();
+
+    assert_eq!(test_bus.read_byte(0), 0x31);
+    assert_eq!(test_bus.read_byte(1), 0xFE);
+    assert_eq!(test_bus.read_byte(2), 0xFF);
 }
