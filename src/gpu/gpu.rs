dba0a9c77e425bb27cde97aac79c27ca20359121
Grace Tveit <grace.tveit@gmail.com>
2022-11-03 15:00:23 -0700

    feat: add Tile struct, read/write vram

--- src/gpu/gpu.rs
+++ src/gpu/gpu.rs
@@ -1,11 +1,15 @@
-struct GPU {
+use super::tile::Tile;
+
+pub struct GPU {
     mode: GPUMode,
     mode_clock: u16,
     line: u8,
+    tileset: [Tile; 384],
+    vram: [u8; 0x2000],
 }
 
 impl GPU {
-    fn step(&mut self, t: u16) {
+    pub fn step(&mut self, t: u16) {
         self.mode_clock += t;
         match self.mode {
             GPUMode::OAMRead => {
@@ -56,6 +60,41 @@ impl GPU {
             }
         }
     }
+
+    pub fn read_vram(&self, addr: u16) -> u8 {
+        self.vram[addr as usize]
+    }
+
+    pub fn write_vram(&mut self, index: u16, value: u8) {
+        self.vram[index as usize] = value;
+
+        // If our index is greater than 0x1800, we're not writing to the tile
+        // set storage, so we can just return
+        if index >= 0x1800 {
+            return;
+        }
+
+        // Tile rows are encoded in two bytes with the first byte always on an
+        // even address. Bitwise ANDing the address with 0x0FFFE gives us the
+        // address of the first byte.
+        // For example: `12 & 0xFFFE == 12` and `13 & 0xFFFE == 12`
+        let normalized_index = index & 0xFFFE;
+
+        // First we need to get the two bytes that encode the tile row.
+        let byte1 = self.vram[normalized_index as usize];
+        let byte2 = self.vram[normalized_index as usize + 1];
+
+        let tile_index = index / 16;
+        let row_index = (index % 16) / 2;
+
+        self.tileset[tile_index as usize].update({ (byte1 as u16) << 8 | byte2 as u16 }, row_index)
+    }
+
+    fn reset_tileset(&mut self) {
+        for x in &mut self.tileset {
+            x.reset()
+        }
+    }
 }
 
 enum GPUMode {
@@ -63,4 +102,21 @@ enum GPUMode {
     VRAMRead,
     HBlank,
     VBlank,
+}
+
+#[cfg(test)]
+use super::tile::Color;
+#[test]
+fn test_write_vram() {
+    let mut test_gpu = GPU {
+        mode: GPUMode::OAMRead,
+        mode_clock: 0,
+        line: 0,
+        tileset: [Tile::new(); 384],
+        vram: [0; 0x2000],
+    };
+
+    test_gpu.write_vram(0x0010, 0xC6);
+
+    assert!(matches!(test_gpu.tileset[1].parse(0, 0), Color::DG))
 }
---
+++ src/gpu/tile.rs
@@ -0,0 +1,68 @@
+#[derive(Copy, Clone)]
+pub struct Tile {
+    lines: [u16; 8]
+}
+
+impl Tile {
+    pub fn new() -> Tile {
+        Tile {
+            lines: [0; 8]
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.lines = [0; 8];
+    }
+
+    pub fn update(&mut self, value: u16, i: u16) {
+        self.lines[i as usize] = value;
+    }
+
+    pub fn parse(&self, x: u8, y:u8) -> Color {
+        Tile::parse_byte(self.lines[y as usize], x)
+    }
+
+    fn parse_byte(word: u16, n: u8) -> Color {
+        let ms_byte = (word >> 8) as u8;
+        let ls_byte = (word & 0x00FF) as u8;
+
+        let ms_byte_bit = ((ms_byte >> 7 -n) & 0x01) == 1;
+        let ls_byte_bit = ((ls_byte >> 7 -n) & 0x01) == 1;
+
+        match (ms_byte_bit, ls_byte_bit) {
+            (false, false) => Color::B,
+            (true, false) => Color::DG,
+            (false, true) => Color::LG,
+            (true, true) => Color::W
+        }
+    }
+}
+
+pub enum Color {
+    W,
+    LG,
+    DG,
+    B
+}
+
+#[cfg(test)]
+#[test]
+fn test_parse() {
+    let sprite = Tile {
+        lines: [
+            0x7C7C,
+            0x00C6,
+            0xC600,
+            0x00FE,
+            0xC6C6,
+            0x00C6,
+            0xC600,
+            0x0000
+        ]
+    };
+
+    assert!(matches!(sprite.parse(0, 0), Color::B));
+    assert!(matches!(sprite.parse(1, 0), Color::W));
+    assert!(matches!(sprite.parse(0, 1), Color::LG));
+    assert!(matches!(sprite.parse(0, 2), Color::DG));
+}
