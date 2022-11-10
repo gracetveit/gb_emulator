daed2c370e4c0314c8db0619b4464b560a94c311
Grace Tveit <grace.tveit@gmail.com>
2022-11-10 08:04:58 -0800

    style: format registerss

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -6,7 +6,7 @@ pub struct Registers {
     pub e: u8,
     pub f: FlagsRegister,
     pub h: u8,
-    pub l: u8
+    pub l: u8,
 }
 
 impl Registers {
@@ -26,8 +26,7 @@ impl Registers {
     pub fn get_bc(&self) -> u16 {
         // pushes the 8 digits of b to the last 8 significant digits, and using
         // a bitwise OR operator, adds c as the first 8 digits
-        (self.b as u16) << 8
-        | self.c as u16
+        (self.b as u16) << 8 | self.c as u16
     }
 
     pub fn set_bc(&mut self, value: u16) {
@@ -40,8 +39,7 @@ impl Registers {
     }
 
     pub fn get_af(&self) -> u16 {
-        (self.a as u16) << 8
-        | (u8::from(self.f)) as u16
+        (self.a as u16) << 8 | (u8::from(self.f)) as u16
     }
 
     pub fn set_af(&mut self, value: u16) {
@@ -50,8 +48,7 @@ impl Registers {
     }
 
     pub fn get_de(&self) -> u16 {
-        (self.d as u16) << 8
-        | self.e as u16
+        (self.d as u16) << 8 | self.e as u16
     }
 
     pub fn set_de(&mut self, value: u16) {
@@ -60,8 +57,7 @@ impl Registers {
     }
 
     pub fn get_hl(&self) -> u16 {
-        (self.h as u16) << 8
-        | self.l as u16
+        (self.h as u16) << 8 | self.l as u16
     }
 
     pub fn set_hl(&mut self, value: u16) {
@@ -80,7 +76,7 @@ pub struct FlagsRegister {
     pub zero: bool,
     pub subtract: bool,
     pub half_carry: bool,
-    pub carry: bool
+    pub carry: bool,
 }
 
 impl std::convert::From<FlagsRegister> for u8 {
@@ -96,10 +92,10 @@ impl std::convert::From<FlagsRegister> for u8 {
     /// 1111 0000
     /// ```
     fn from(flag: FlagsRegister) -> u8 {
-        (if flag.zero {1} else {0}) << ZERO_FLAG_BYTE_POSTION |
-        (if flag.subtract {1} else {0}) << SUBTRACT_FLAG_BYTE_POSTIONG |
-        (if flag.half_carry {1} else {0}) << HALF_CARRY_FLAG_BYTE_POSITION |
-        (if flag.carry {1} else {0}) << CARRY_FLAG_BYTE_POSITION
+        (if flag.zero { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSTION
+            | (if flag.subtract { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSTIONG
+            | (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION
+            | (if flag.carry { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
     }
 }
 
@@ -120,7 +116,7 @@ impl std::convert::From<u8> for FlagsRegister {
             zero,
             subtract,
             half_carry,
-            carry
+            carry,
         }
     }
 }
