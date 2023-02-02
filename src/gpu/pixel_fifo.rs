e193e7698c1df68ec9396092dfc4e9f8d0b88708
Grace Tveit <grace.tveit@gmail.com>
2023-02-01 16:55:24 -0800

    feat: working on FIFO `step()`

--- src/gpu/pixel_fifo.rs
+++ src/gpu/pixel_fifo.rs
@@ -27,8 +27,44 @@ impl PixelFIFO {
         }
     }
 
-    pub fn step(&mut self) {
+    pub fn step(&mut self, line: [[u8; 4]; 160]) -> [[u8; 4]; 160] {
         // TODO: Outline Step Function
+        let mut new_line = line;
+        // Push
+        if self.t % 2 == 0 && self.t <= 4 {
+            // Conditional fetch steps
+            self.fetcher.step();
+        }
+
+        if self.t == 7 {
+            self.t = 0;
+        } else {
+            self.t += 1;
+        }
+
+        new_line
+    }
+
+    fn push(&mut self) -> Option<[u8; 4]> {
+        // !!!
+        // FIFO must contain more than 8 pixels to be able to push something out
+        // !!!
+        if let None = self.fifo[8] {
+            return None;
+        };
+        match self.fifo[0] {
+            None => {
+                // None
+                todo!()
+            }
+            Some(pixel_data) => {
+                // TODO: convert pixel data & pallette info into actual RGBA
+                self.fifo[0] = None;
+                self.fifo.rotate_left(1);
+                todo!()
+
+            }
+        }
     }
 
     fn sprite_check(&mut self) -> Option<&Sprite> {
@@ -56,6 +92,22 @@ impl PixelFIFO {
             }
         }
         active_sprite
+    }
+
+    fn sprite_overlay(&mut self, sprite_pixels: [PixelData; 8]) {
+        for i in 0..9 {
+            match self.fifo[i] {
+                None => panic!("Attempting to overlay a sprite onto empty pixels"),
+                Some(base_pixel) => match base_pixel.pallette {
+                    Pallette::Background => {
+                        if sprite_pixels[i].data != 0 {
+                            self.fifo[i] = Some(sprite_pixels[i]);
+                        }
+                    }
+                    _ => {}
+                },
+            }
+        }
     }
 }
 
@@ -86,25 +138,21 @@ impl Fetcher {
         self.mode = mode
     }
 
-    pub fn step(&mut self) -> Option<[PixelData; 8]> {
+    pub fn step(&mut self) {
         match self.tile_addr {
             None => {
                 // TODO: Fetch tile_addr
-                None
             }
             Some(tile_addr) => match self.data_0 {
                 None => {
                     // TODO: Fetch data_0
-                    None
                 }
                 Some(data_0) => match self.data_1 {
                     None => {
                         // TODO: Fetch data_1
-                        None
                     }
                     Some(data_1) => {
                         // TOPDO: Returns Pixel Data
-                        Some(self.construct_pixel_data(data_0, data_1))
                     }
                 },
             },
